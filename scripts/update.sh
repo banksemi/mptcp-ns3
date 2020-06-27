ab387c305d3496abe912a3aa651caa6537a621f7
Lee Seunghwa <banksemi@naver.com>
2020-06-27 23:44:09 +0900

    Add comment to /scripts/update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,6 +2,11 @@
 
 git stash
 git pull
-# git stash pop
+# git stash pop # git stash apply; git stash drop;
+
+# stash 리스트 출력
+git stash list
+
+echo "If you restore your works, Please enter 'git stash pop'"
 
 ./_update.sh
