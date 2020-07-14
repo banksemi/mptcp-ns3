7f49e206b1bdaa8315d124d24add94c748cb67c2
Seunghwa <banksemi@naver.com>
2020-07-14 07:11:15 -0700

    Fix 'don't use slow path in new packet scheduling'

--- dce/source/net-next-nuse-4.4.0/net/mptcp/mptcp_only_fast_sched.c
+++ dce/source/net-next-nuse-4.4.0/net/mptcp/mptcp_only_fast_sched.c
@@ -3,27 +3,22 @@
 #include <linux/module.h>
 #include <net/mptcp.h>
 
+/* Same as mptcp_dss_len from mptcp_output.c */
+static const int mptcp_dss_len = MPTCP_SUB_LEN_DSS_ALIGN +
+				 MPTCP_SUB_LEN_ACK_ALIGN +
+				 MPTCP_SUB_LEN_SEQ_ALIGN;
+
 static DEFINE_SPINLOCK(mptcp_sched_list_lock);
 static LIST_HEAD(mptcp_sched_list);
 
 struct defsched_priv {
 	u32	last_rbuf_opti;
-	bool rtt_check;
-	struct sk_buff *rtt_check_buf;
 };
 
 static struct defsched_priv *defsched_get_priv(const struct tcp_sock *tp)
 {
 	return (struct defsched_priv *)&tp->mptcp->mptcp_sched[0];
 }
-
-static struct defsched_priv *metasched_get_priv(const struct tcp_sock *meta_tp)
-{
-	struct mptcp_cb *mpcb = meta_tp->mpcb;
-	struct tcp_sock *first_tp = mpcb->connection_list; // first connection
-	return defsched_get_priv(first_tp);
-}
-
 
 static bool mptcp_is_def_unavailable(struct sock *sk)
 {
@@ -129,6 +124,35 @@ static int mptcp_dont_reinject_skb(const struct tcp_sock *tp, const struct sk_bu
 		mptcp_pi_to_flag(tp->mptcp->path_index) & TCP_SKB_CB(skb)->path_mask;
 }
 
+/* Function that returns the slowest pass of established subflows */
+static struct sock *get_slow_socket(struct mptcp_cb *mpcb) {
+	struct sock *sk;
+
+	/* Initialization to find fast flow*/
+	struct sock *fastsk = NULL, *slowsk = NULL;
+	u32 fast_srtt = 0xffffffff, slow_srtt = 0;
+
+	mptcp_for_each_sk(mpcb, sk) {
+		struct tcp_sock *tp = tcp_sk(sk);
+		bool unused = false;
+
+		/* Find Fast Flow in selection flows */
+		if (!mptcp_is_def_unavailable(sk) && tcp_sk(sk)->srtt_us) {
+			if (fast_srtt > tp->srtt_us) {
+				fast_srtt = tp->srtt_us;
+				fastsk = sk;
+			}
+			if (slow_srtt < tp->srtt_us) {
+				slow_srtt = tp->srtt_us;
+				slowsk = sk;
+			}
+		}
+	}
+	if (mpcb->cnt_established >= 2 && (fastsk && slowsk) && fastsk != slowsk)
+		return slowsk;
+	return NULL;
+}
+
 static bool subflow_is_backup(const struct tcp_sock *tp)
 {
 	return tp->mptcp->rcv_low_prio || tp->mptcp->low_prio;
@@ -153,10 +177,6 @@ static struct sock
 	bool found_unused_una = false;
 	struct sock *sk;
 
-	/* Initialization to find fast flow*/
-	struct sock *fastsk = NULL, *slowsk = NULL;
-	u32 fast_srtt = 0xffffffff, slow_srtt = 0;
-
 	mptcp_for_each_sk(mpcb, sk) {
 		struct tcp_sock *tp = tcp_sk(sk);
 		bool unused = false;
@@ -164,18 +184,6 @@ static struct sock
 		/* First, we choose only the wanted sks */
 		if (!(*selector)(tp))
 			continue;
-
-		/* Find Fast Flow in selection flows */
-		if (!mptcp_is_def_unavailable(sk) && tcp_sk(sk)->srtt_us) {
-			if (fast_srtt > tp->srtt_us) {
-				fast_srtt = tp->srtt_us;
-				fastsk = sk;
-			}
-			if (slow_srtt < tp->srtt_us) {
-				slow_srtt = tp->srtt_us;
-				slowsk = sk;
-			}
-		}
 
 		if (!mptcp_dont_reinject_skb(tp, skb))
 			unused = true;
@@ -229,27 +237,8 @@ static struct sock
 		else
 			*force = false;
 	}
-
-	/* Select Fast flow or Slow Flow */
-	if (skb && mpcb->cnt_established >= 2 && bestsk && (fastsk && slowsk) && fastsk != slowsk) { 
-		struct sock *meta_sk = mptcp_meta_sk(bestsk);
-		struct tcp_sock *meta_tp = tcp_sk(meta_sk);
-		struct tcp_sock *fasttp = tcp_sk(fastsk);
-		struct tcp_sock *slowtp = tcp_sk(slowsk); 
-	
-		struct defsched_priv *dsp = metasched_get_priv(meta_tp);
-		if (skb == dsp->rtt_check_buf) { 
-			//return slowtp;
-			// 이미 요청 했을때는 일딴 slow로 빠르게 보내서 reinject queue때문에 fast 플로우로 갈려는 패킷을 막는 현상이 발생하지 않도록 해야함. (무시)
-			if (!tcp_packets_in_flight(slowtp)) { 
-				return slowtp; 
-			} 
-		} else if (bestsk == slowsk) { 
-			if (!tcp_packets_in_flight(slowtp))  
-				dsp->rtt_check = true; // 다음번 세그먼트는 RTT 체크용으로 요청 
-			return NULL; 
-		} 
-	}
+	if (TCP_SKB_CB(skb)->path_mask == 0 && bestsk == get_slow_socket(mpcb))
+		return NULL;
 
 	return bestsk;
 }
@@ -395,9 +384,9 @@ retrans:
  */
 static struct sk_buff *__mptcp_next_segment(struct sock *meta_sk, int *reinject)
 {
-	const struct mptcp_cb *mpcb = tcp_sk(meta_sk)->mpcb;
+	struct mptcp_cb *mpcb = tcp_sk(meta_sk)->mpcb;
 	struct sk_buff *skb = NULL;
-
+begin:
 	*reinject = 0;
 
 	/* If we are in fallback-mode, just take from the meta-send-queue */
@@ -408,6 +397,25 @@ static struct sk_buff *__mptcp_next_segment(struct sock *meta_sk, int *reinject)
 
 	if (skb) {
 		*reinject = 1;
+		struct sock *subsk = get_available_subflow(meta_sk,
+								   skb,
+								   false);
+		struct tcp_sock *tp = tcp_sk(subsk);
+		if (!subsk) {
+			/* There is no available subflow */
+			skb_unlink(skb, &mpcb->reinject_queue);
+			__kfree_skb(skb);
+			goto begin;
+		}
+
+		if (TCP_SKB_CB(skb)->path_mask == 0 ||
+			TCP_SKB_CB(skb)->path_mask &
+			mptcp_pi_to_flag(tp->mptcp->path_index)) {
+			/* The specified path cannot be used. */
+			skb_unlink(skb, &mpcb->reinject_queue);
+			__kfree_skb(skb);
+			goto begin;
+		}
 	} else {
 		skb = tcp_send_head(meta_sk);
 
@@ -459,10 +467,31 @@ static struct sk_buff *mptcp_next_segment(struct sock *meta_sk,
 			return NULL;
 	}
 
-	if (TCP_SKB_CB(skb)->path_mask == 0)
-	{
-		struct defsched_priv *dsp = metasched_get_priv(tcp_sk(meta_sk));
-		dsp->rtt_check_buf = skb;
+	const struct tcp_sock *meta_tp = tcp_sk(meta_sk);
+	struct mptcp_cb *mpcb = meta_tp->mpcb;
+	struct sock *slowsk = get_slow_socket(mpcb);
+	if (slowsk != NULL) {
+		struct tcp_sock *slowtp = tcp_sk(slowsk);
+
+		/* If there are no packets in flight on the slow path, and the packet to be sent now is not an injected packet */
+		if (!tcp_packets_in_flight(slowtp) && TCP_SKB_CB(skb)->path_mask == 0) {
+			struct mptcp_cb *mpcb = meta_tp->mpcb;
+			struct sock *slowsk = get_slow_socket(mpcb);
+			/* Copy SKB */
+			struct sk_buff *copy_skb = pskb_copy_for_clone(skb, GFP_ATOMIC);
+			if (likely(copy_skb)) { /* SKB isn't NULL */
+				copy_skb->sk = meta_sk;
+				if (!after(TCP_SKB_CB(copy_skb)->end_seq, meta_tp->snd_una)) {
+					__kfree_skb(copy_skb);
+				} else {
+					memset(TCP_SKB_CB(copy_skb)->dss, 0 , mptcp_dss_len);
+					TCP_SKB_CB(copy_skb)->path_mask = mptcp_pi_to_flag(tcp_sk(slowsk)->mptcp->path_index);
+					TCP_SKB_CB(copy_skb)->path_mask ^= -1u;
+					TCP_SKB_CB(copy_skb)->dss[1] = 1;
+					skb_queue_tail(&mpcb->reinject_queue, copy_skb);
+				}
+			}
+		}
 	}
 
 	/* No splitting required, as we will only send one single segment */
@@ -503,8 +532,6 @@ static void defsched_init(struct sock *sk)
 	struct defsched_priv *dsp = defsched_get_priv(tcp_sk(sk));
 
 	dsp->last_rbuf_opti = tcp_time_stamp;
-	dsp->rtt_check = false;
-	dsp->rtt_check_buf = NULL;
 }
 
 struct mptcp_sched_ops mptcp_only_fast = {
