93e061f968f67df18f2258b96eff64768cccf9ba
Seunghwa <banksemi@naver.com>
2020-07-16 06:33:03 -0700

    Change bandwidth of RTT_changed scenario

--- dce/source/ns-3-dce/myscripts/test-standard.cc
+++ dce/source/ns-3-dce/myscripts/test-standard.cc
@@ -196,7 +196,7 @@ int main (int argc, char *argv[]) {
     if (rtt_change) {
         for(int i = 2; i<32; i += 1) {
             Simulator::Schedule (Seconds (i), &ChangeRTT, (_switch) % 2, StringValue("100Mbps"), StringValue("5ms")); // start UE movement
-            Simulator::Schedule (Seconds (i), &ChangeRTT, (_switch + 1) % 2, StringValue("100Mbps"), StringValue("30ms")); // start UE movement
+            Simulator::Schedule (Seconds (i), &ChangeRTT, (_switch + 1) % 2, StringValue("30Mbps"), StringValue("30ms")); // start UE movement
             _switch++;
 	    }
     } else {
