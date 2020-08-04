2a035a4ffcb3a948ff2fa466c53b68eebe00c2aa
Seunghwa <banksemi@naver.com>
2020-08-03 19:42:03 -0700

    Add option to change bandwidth

--- dce/source/ns-3-dce/myscripts/test-standard.cc
+++ dce/source/ns-3-dce/myscripts/test-standard.cc
@@ -211,30 +211,32 @@ int main (int argc, char *argv[]) {
     apps = dce.Install (nodes.Get (1));
     apps.Start (Seconds (1.5));
 
-
+    StringValue set_bandwidth1 = "100Mbps";
     int set_rtt1 = 5;
+
+    StringValue set_bandwidth2 = "30Mbps";
     int set_rtt2 = 50;
     int _switch = 0;
     bool pacing = true;
 
-    Simulator::Schedule(Seconds(1), &ChangeRTT, (_switch) % 2 , StringValue("100Mbps"), StringValue(std::to_string(set_rtt1) + "ms")); // start UE movement
-    Simulator::Schedule(Seconds(1), &ChangeRTT, (_switch + 1) % 2, StringValue("100Mbps"), StringValue(std::to_string(set_rtt2) + "ms")); // start UE movement
+    Simulator::Schedule(Seconds(1), &ChangeRTT, (_switch) % 2 , set_bandwidth1, StringValue(std::to_string(set_rtt1) + "ms")); // start UE movement
+    Simulator::Schedule(Seconds(1), &ChangeRTT, (_switch + 1) % 2, set_bandwidth2, StringValue(std::to_string(set_rtt2) + "ms")); // start UE movement
     _switch++;
 
     if (rtt_change) {
-        for(int i = 12; i<32; i += 1) {
+        for(int i = 7; i<32; i += 5) {
             if (pacing == false) {
-                Simulator::Schedule (Seconds (i), &ChangeRTT, (_switch) % 2, StringValue("100Mbps"), StringValue(std::to_string(set_rtt1) + "ms")); // start UE movement
-                Simulator::Schedule (Seconds (i), &ChangeRTT, (_switch + 1) % 2, StringValue("100Mbps"), StringValue(std::to_string(set_rtt2) + "ms")); // start UE movement
+                Simulator::Schedule (Seconds (i), &ChangeRTT, (_switch) % 2, set_bandwidth1, StringValue(std::to_string(set_rtt1) + "ms")); // start UE movement
+                Simulator::Schedule (Seconds (i), &ChangeRTT, (_switch + 1) % 2, set_bandwidth2, StringValue(std::to_string(set_rtt2) + "ms")); // start UE movement
             } else {
                 int t = i * 1000;
                 for (int x = t - set_rtt1; x <= t; x++) {
-                    StringValue delay1_ms = StringValue(to_string_with_precision<float>(Delay(t, set_rtt1, set_rtt2, x)) + "ms");
-                    Simulator::Schedule (Seconds (x / 1000.0), &ChangeRTT, (_switch + 1) % 2, StringValue("100Mbps"), delay1_ms); // start UE movement
+                    StringValue delay_ms = StringValue(to_string_with_precision<float>(Delay(t, set_rtt1, set_rtt2, x)) + "ms");
+                    Simulator::Schedule (Seconds (x / 1000.0), &ChangeRTT, (_switch + 1) % 2, set_bandwidth2, delay_ms); // start UE movement
                 }
                 for (int x = t - set_rtt2; x <= t; x++) {
-                    StringValue delay2_ms = StringValue(to_string_with_precision<float>(Delay(t, set_rtt2, set_rtt1, x)) + "ms");
-                    Simulator::Schedule (Seconds (x / 1000.0), &ChangeRTT, (_switch) % 2, StringValue("100Mbps"), delay2_ms); // start UE movement
+                    StringValue delay_ms = StringValue(to_string_with_precision<float>(Delay(t, set_rtt2, set_rtt1, x)) + "ms");
+                    Simulator::Schedule (Seconds (x / 1000.0), &ChangeRTT, (_switch) % 2, set_bandwidth1, delay_ms); // start UE movement
                 }
             }
             _switch++;
