074ed5739b2fb94289b6ca32e55cc50e1b9ce28d
Seunghwa <banksemi@naver.com>
2020-09-07 00:45:49 -0700

    Implement one wifi device

--- dce/source/ns-3-dce/myscripts/11ac.cc
+++ dce/source/ns-3-dce/myscripts/11ac.cc
@@ -7,17 +7,20 @@
 #include "ns3/netanim-module.h"
 #include "ns3/constant-position-mobility-model.h"
 
-/*
+
+#include "ns3/network-module.h"
+#include "ns3/core-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/dce-module.h"
+#include "ns3/point-to-point-module.h"
 #include "ns3/wifi-module.h"
 #include "ns3/lte-module.h"
-#include "ns3/buildings-module.h"
-#include "ns3/mmwave-module.h"
 #include "ns3/mobility-module.h"
 #include "ns3/applications-module.h"
 #include "ns3/netanim-module.h"
 #include "ns3/constant-position-mobility-model.h"
 #include "ns3/config-store-module.h"
-*/
+
 #include <iostream>
 #include <ctime>
 #include <stdlib.h>
@@ -44,11 +47,13 @@ void ChangeRTT(int device, StringValue BandRate, StringValue RTT) {
     // /ChannelList/3: 노드0 -> ... -> 라우터 -> *path2* -> 노드1
 
     if (device == 0) {    
-        Config::Set("/NodeList/1/DeviceList/0/$ns3::PointToPointNetDevice/DataRate", BandRate);
+        // 0
+        Config::Set("/NodeList/0/DeviceList/0/$ns3::PointToPointNetDevice/DataRate", BandRate);
+        Config::Set("/NodeList/2/DeviceList/0/$ns3::PointToPointNetDevice/DataRate", BandRate);
         Config::Set("/ChannelList/1/$ns3::PointToPointChannel/Delay", RTT);
 	} else {
-        Config::Set("/NodeList/1/DeviceList/1/$ns3::PointToPointNetDevice/DataRate", BandRate);
-        Config::Set("/ChannelList/3/$ns3::PointToPointChannel/Delay", RTT);
+       // Config::Set("/NodeList/0/DeviceList/1/$ns3::PointToPointNetDevice/DataRate", BandRate);
+        //Config::Set("/ChannelList/2/$ns3::PointToPointChannel/Delay", RTT);
 	}
 }
 void setPos (Ptr<Node> n, int x, int y, int z) {
@@ -72,11 +77,11 @@ std::string to_string_with_precision(const T a_value, const int n = 6)
 
 int main (int argc, char *argv[]) {
     LogComponentEnable ("DceMptcpTest", LOG_LEVEL_ALL);
-    uint32_t nRtrs = 2;
+    uint32_t nRtrs = 1;
     CommandLine cmd;
     std::string sched = "only_fast";
     bool rtt_change = true;
-    std::string bandwidth = "1Mbit";
+    std::string bandwidth = "0";
 
     cmd.AddValue ("sched", "sched value", sched);
     cmd.AddValue ("rtt_change", "rtt_change value", rtt_change);
@@ -89,7 +94,8 @@ int main (int argc, char *argv[]) {
     NodeContainer nodes, routers;
     nodes.Create (2);
     routers.Create (nRtrs);
-
+setPos (nodes.Get (0), 50, 0, 0);
+setPos (nodes.Get (1), 50, 0, 0);
     DceManagerHelper dceManager;
     dceManager.SetTaskManagerAttribute ("FiberManagerType", StringValue ("UcontextFiberManager"));
     dceManager.SetNetworkStack ("ns3::LinuxSocketFdFactory", "Library", StringValue ("liblinux.so"));
@@ -99,6 +105,20 @@ int main (int argc, char *argv[]) {
     LinuxStackHelper stack;
     stack.Install (nodes);
     stack.Install (routers);
+
+
+    Ssid ssid = Ssid ("ns3-wifi");
+    WifiHelper wifi = WifiHelper::Default ();
+    YansWifiPhyHelper phy = YansWifiPhyHelper::Default ();
+    YansWifiChannelHelper phyChannel = YansWifiChannelHelper::Default ();
+    NqosWifiMacHelper mac;
+    phy.Set ("Antennas", UintegerValue (2));
+    phy.Set ("MaxSupportedTxSpatialStreams", UintegerValue (2));
+    phy.Set ("MaxSupportedRxSpatialStreams", UintegerValue (2));
+    phy.SetChannel (phyChannel.Create ());
+    wifi.SetRemoteStationManager ("ns3::MinstrelHtWifiManager", "RtsCtsThreshold", UintegerValue (65535), "PrintStats", BooleanValue (true));
+    mac.SetType ("ns3::AdhocWifiMac");
+    wifi.SetStandard (WIFI_PHY_STANDARD_80211ac);
 
 
     PointToPointHelper pointToPoint;
@@ -111,8 +131,8 @@ int main (int argc, char *argv[]) {
     for (uint32_t i = 0; i < nRtrs; i++) {
         // Left 링크의 pointToPoint 속성은 DCE 스케줄에 의해 나중에 다시 세팅됨.
         // Left link
-        pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("10Gbps"));
-        pointToPoint.SetChannelAttribute ("Delay", StringValue ("0ms")); // 별도 옵션
+        pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("1Mbps"));
+        pointToPoint.SetChannelAttribute ("Delay", StringValue ("50ms")); // 별도 옵션
         devices1 = pointToPoint.Install (nodes.Get (0), routers.Get (i));
         // Assign ip addresses
         Ipv4InterfaceContainer if1 = address1.Assign (devices1);
@@ -131,15 +151,65 @@ int main (int argc, char *argv[]) {
         cmd_oss << "route add 10.1."<< i <<".0/24 via " << if1.GetAddress (1, 0) << " dev sim0";
         LinuxStackHelper::RunIp (routers.Get (i), Seconds (0.2), cmd_oss.str ().c_str ());
 
+
         // Right link
-        pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("10Gbps"));
-        pointToPoint.SetChannelAttribute ("Delay", StringValue ("0ns"));
-        devices2 = pointToPoint.Install (nodes.Get (1), routers.Get (i));
+        //pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("10Gbps"));
+        //pointToPoint.SetChannelAttribute ("Delay", StringValue ("0ns"));
+        
+        //devices2 = pointToPoint.Install (nodes.Get (1), routers.Get (i));
+        devices2 = wifi.Install (phy, mac, NodeContainer (nodes.Get (1), routers.Get (i)));
+
+        Ptr<WifiNetDevice> wnd = devices2.Get (0)->GetObject<WifiNetDevice> ();
+        Ptr<WifiPhy> wp = wnd->GetPhy ();
+        wp->SetChannelWidth (160);
+
+
+
+        /*
+        NodeContainer wifiNodes;
+        wifiNodes.Add(nodes.Get (1));
+
+        NodeContainer wifiNodes2;
+        wifiNodes2.Add(routers.Get (i));
+
+        mac.SetType ("ns3::StaWifiMac",
+             "Ssid", SsidValue (ssid));
+
+        apdevices = wifi.Install (phy, mac, wifiNodes);
+
+
+        mac.SetType ("ns3::ApWifiMac",
+             "Ssid", SsidValue (ssid));
+
+        stadevices = wifi.Install (phy, mac, wifiNodes2);
+
+
+        Ptr<NetDevice> ndClient = apdevices.Get (0);
+        Ptr<NetDevice> ndServer = stadevices.Get (0);
+        Ptr<WifiNetDevice> wndClient = ndClient->GetObject<WifiNetDevice> ();
+        Ptr<WifiNetDevice> wndServer = ndServer->GetObject<WifiNetDevice> ();
+        Ptr<WifiPhy> wifiPhyPtrClient = wndClient->GetPhy ();
+        Ptr<WifiPhy> wifiPhyPtrServer = wndServer->GetPhy ();
+
+
+        wifiPhyPtrClient->SetChannelWidth (40);
+        wifiPhyPtrServer->SetChannelWidth (40);
+        */
+
+        /*
         // Assign ip addresses
-        Ipv4InterfaceContainer if2 = address2.Assign (devices2);
-        address2.NewNetwork ();
+        
+        */
         // setup ip routes
         cmd_oss.str ("");
+        Ipv4InterfaceContainer if2 = address2.Assign (devices2);
+        address2.NewNetwork ();
+        Ipv4Address ip1 =  if2.GetAddress (0, 0) ; // apdevices.Get (0)->GetAddress (); // 
+        
+        Ipv4Address ip2 = if2.GetAddress (1, 0) ;// stadevices.Get (0)->GetAddress (); // 
+
+
+
         cmd_oss << "rule add from " << if2.GetAddress (0, 0) << " table " << (i+1);
         LinuxStackHelper::RunIp (nodes.Get (1), Seconds (0.1), cmd_oss.str ().c_str ());
         cmd_oss.str ("");
@@ -152,7 +222,7 @@ int main (int argc, char *argv[]) {
         cmd_oss << "route add 10.2."<< i <<".0/24 via " << if2.GetAddress (1, 0) << " dev sim1";
         LinuxStackHelper::RunIp (routers.Get (i), Seconds (0.2), cmd_oss.str ().c_str ());
 
-        setPos (routers.Get (i), 50, i * 20, 0);
+        setPos (routers.Get (i), 50, 35, 0);
     }
 
   // default route
@@ -161,7 +231,7 @@ int main (int argc, char *argv[]) {
     LinuxStackHelper::RunIp (nodes.Get (0), Seconds (0.1), "rule show");
 
 
-    stack.SysctlSet(nodes, ".net.mptcp.mptcp_enabled", "1");
+    stack.SysctlSet(nodes, ".net.mptcp.mptcp_enabled", "0");
     stack.SysctlSet(nodes, ".net.mptcp.mptcp_scheduler", sched);
     stack.SysctlSet(nodes, ".net.mptcp.mptcp_checksum", "1");
     
@@ -178,7 +248,7 @@ int main (int argc, char *argv[]) {
     dce.AddArgument ("-i");
     dce.AddArgument ("1.0");
     dce.AddArgument ("--time");
-    dce.AddArgument ("30");
+    dce.AddArgument ("10");
     dce.AddArgument ("--bandwidth");
     dce.AddArgument (bandwidth);
     //dce.AddArgument ("--json");
@@ -201,8 +271,8 @@ int main (int argc, char *argv[]) {
     StringValue set_bandwidth1 = StringValue("100Mbps");
     int set_rtt1 = 5;
 
-    StringValue set_bandwidth2 = StringValue("30Mbps");
-    int set_rtt2 = 50;
+    StringValue set_bandwidth2 = StringValue("100Mbps");
+    int set_rtt2 = 5;
     int _switch = 0;
     bool pacing = true;
 
