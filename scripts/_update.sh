c44f671475df6792daa2a1dbc9eddd7a30c34f22
Seunghwa Lee <banksemi@naver.com>
2023-08-28 08:24:45 +0900

    Modify to include iperf3 in update script

--- scripts/_update.sh
+++ scripts/_update.sh
@@ -5,6 +5,7 @@ function CodeCopy() {
 	echo Copy: '../dce/source/'$1 '../../source/'$1'/../' -R -f
 }
 
+CodeCopy "iperf3"
 CodeCopy "ns-3.28/src/mmwave/model"
 CodeCopy "net-next-nuse-4.4.0/net"
 CodeCopy "ns-3-dce/myscripts"
