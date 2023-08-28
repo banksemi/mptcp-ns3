a2943004d6809f19cdcc47d7b80187bd608908cd
Seunghwa Lee <banksemi@naver.com>
2023-08-28 12:45:04 +0900

    Update full_test.sh

--- scripts/full_test.sh
+++ scripts/full_test.sh
@@ -1,18 +1,26 @@
 ./_update.sh
 ./linux-build.sh
-
+./iperf3-build.sh
 
 rm ../logs/*.txt
+rm ../logs_receiver/*.txt
 
+rm ../*.pcap
+mkdir ../../pcap/logs
+mkdir ../../pcap/logs_receiver
 
-./dce-run.sh "test-standard --sched=default"
-cp ../../source/ns-3-dce/files-1/var/log/messages ../logs/default.txt
+./dce-run.sh "test-standard --sched=default --bandwidth=1Mbit"
+cp ../../source/ns-3-dce/files-0/var/log/messages ../../pcap/logs_receiver/default.txt
+cp ../../source/ns-3-dce/files-1/var/log/messages ../../pcap/logs/default.txt
 
-./dce-run.sh "test-standard --sched=blest"
-cp ../../source/ns-3-dce/files-1/var/log/messages ../logs/blest.txt
+./dce-run.sh "test-standard --sched=blest --bandwidth=1Mbit"
+cp ../../source/ns-3-dce/files-0/var/log/messages ../../pcap/logs_receiver/blest.txt
+cp ../../source/ns-3-dce/files-1/var/log/messages ../../pcap/logs/blest.txt
 
-./dce-run.sh "test-standard --sched=redundant"
-cp ../../source/ns-3-dce/files-1/var/log/messages ../logs/redundant.txt
+./dce-run.sh "test-standard --sched=redundant --bandwidth=1Mbit"
+cp ../../source/ns-3-dce/files-0/var/log/messages ../../pcap/logs_receiver/redundant.txt
+cp ../../source/ns-3-dce/files-1/var/log/messages ../../pcap/logs/redundant.txt
 
-./dce-run.sh "test-standard --sched=only_fast"
-cp ../../source/ns-3-dce/files-1/var/log/messages ../logs/only_fast.txt
+./dce-run.sh "test-standard --sched=only_fast --bandwidth=1Mbit"
+cp ../../source/ns-3-dce/files-0/var/log/messages ../../pcap/logs_receiver/only_fast.txt
+cp ../../source/ns-3-dce/files-1/var/log/messages ../../pcap/logs/only_fast.txt
